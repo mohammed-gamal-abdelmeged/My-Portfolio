44f04f6ca2a6eab13346a1b05a0d7528a01dcc96
mohammed-gamal-abdelmeged <modygamal355@gmail.com>
2025-09-16 17:25:07 +0300

    My-Portfolio

---
+++ main.js
@@ -0,0 +1,78 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+
+
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
+
+
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .portfolio-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+
+
+const typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer" , "Ai student"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+
+
+const homeBg = document.querySelector('.home-bg');
+const starCount = 150;
+
+for (let i = 0; i < starCount; i++) {
+  const star = document.createElement('div');
+  star.classList.add('star');
+    const size = Math.random() * 3 + 1;
+  star.style.width = `${size}px`;
+  star.style.height = `${size}px`;
+    star.style.top = `${Math.random() * 100}%`;
+  star.style.left = `${Math.random() * 100}%`;
+    star.style.animationDuration = `${Math.random() * 7 + 8}s`;
+  
+  homeBg.appendChild(star);
+}
+
